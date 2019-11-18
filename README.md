03b0ecbb5f191a13a208bf7f0595fa29b3b85d81
maphnew <zcm3118@gmail.com>
2019-11-18 19:04:58 +0900

    32. Customizing HTTP Requests

--- README.md
+++ README.md
@@ -516,6 +516,24 @@ request({ url: url }, (error, response) => {
 32. Customizing HTTP Requests
 18분
 
+- json: true
+- ?units=si&lang=ko
+
+```JavaScript
+//app.js
+const request = require('request')
+
+const url = 'https://api.darksky.net/forecast/a36cc7f940e72f0b614e6e427e964599/37.8267,-122.4233?units=si&lang=ko'
+
+request({ url: url, json: true}, (error, response) => {
+    temperature = response.body.currently.temperature
+    precipProbability = response.body.currently.precipProbability
+    console.log(response.body.daily.data[0].summary + " It is currently " +temperature+ " degrees out. There is a " +precipProbability+"% chance of rain.")
+    // const data = JSON.parse(response.body)
+    // console.log(data.currently)
+})
+```
+
 33. An HTTP Request Challenge
 19분
 
--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,11 @@
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/a36cc7f940e72f0b614e6e427e964599/37.8267,-122.4233'
+const url = 'https://api.darksky.net/forecast/a36cc7f940e72f0b614e6e427e964599/37.8267,-122.4233?units=si&lang=ko'
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
+request({ url: url, json: true}, (error, response) => {
+    temperature = response.body.currently.temperature
+    precipProbability = response.body.currently.precipProbability
+    console.log(response.body.daily.data[0].summary + " It is currently " +temperature+ " degrees out. There is a " +precipProbability+"% chance of rain.")
+    // const data = JSON.parse(response.body)
+    // console.log(data.currently)
 })
