e7677571d5f8a9ab524adade8cec2aa9f6963031
maphnew <zcm3118@gmail.com>
2019-11-19 21:24:19 +0900

    36. Callback Abstraction

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,4 +1,5 @@
 const request = require('request')
+const geocode = require('./utils/geocode')
 
 // // const url = 'https://api.darksky.net/forecast/a36cc7f940e72f0b614e6e427e964599/37.8267?units=si&lang=ko'
 // const url = 'https://api.darksky.net/forecast/a36cc7f940e72f0b614e6e427e964599/37.8267,-122.4233?units=si&lang=ko'
@@ -18,18 +19,25 @@ const request = require('request')
 // Geocoding
 // Address -> Lat/Long -> Weather
 
-// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibWFwaG5ldyIsImEiOiJjazM0OW82Z28wdWpoM2JvYjh5ZThpanNlIn0.HB5kQ-HAANoU2yXPmDXA2w'
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/129999999.json?access_token=pk.eyJ1IjoibWFwaG5ldyIsImEiOiJjazM0OW82Z28wdWpoM2JvYjh5ZThpanNlIn0.HB5kQ-HAANoU2yXPmDXA2w'
+// // const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibWFwaG5ldyIsImEiOiJjazM0OW82Z28wdWpoM2JvYjh5ZThpanNlIn0.HB5kQ-HAANoU2yXPmDXA2w'
+// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/129999999.json?access_token=pk.eyJ1IjoibWFwaG5ldyIsImEiOiJjazM0OW82Z28wdWpoM2JvYjh5ZThpanNlIn0.HB5kQ-HAANoU2yXPmDXA2w'
 
-request({ url: geocodeURL, json: true}, (error, response) => {
-    if (error) {
-        console.log('Unable to connect to location service!')
-    } else if (response.body.features.length === 0) {
-        console.log('Unable to find location. Try another search.')
-    } else {
-        const latitude = response.body.features[0].center[1]
-        const longitude = response.body.features[0].center[0]
-        console.log(latitude, longitude)
-    }
+// request({ url: geocodeURL, json: true}, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to location service!')
+//     } else if (response.body.features.length === 0) {
+//         console.log('Unable to find location. Try another search.')
+//     } else {
+//         const latitude = response.body.features[0].center[1]
+//         const longitude = response.body.features[0].center[0]
+//         console.log(latitude, longitude)
+//     }
     
+// })
+
+
+
+geocode('Boston', (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
 })
