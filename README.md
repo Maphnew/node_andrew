4f03feeb19b64e72e094bf902ee0ca54e8e5e576
maphnew <zcm3118@gmail.com>
2019-12-03 20:46:51 +0900

    85. Data Validation and Sanitization: Part I

--- README.md
+++ README.md
@@ -2134,6 +2134,52 @@ task.save().then((task) => {
 85. Data Validation and Sanitization: Part I
 18분
 
+
+```JavaScript
+//task-manager/src/db/mongoose.js
+
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+    useNweUrlParser: true, 
+    useCreateIndex: true
+})
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
+    }
+})
+
+const me = new User({
+    name: '    Maphnew   ',
+    email: 'MYEMAIL@MAPH.IO    '
+})
+```
+
 86. Data Validation and Sanitization: Part II
 12분
 
--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNweUrlParser: true, 
@@ -7,23 +8,42 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
     }
 })
 
-// const me = new User({
-//     name: 'Maphnew',
-//     age: 'MMM'
-// })
+const me = new User({
+    name: '    Maphnew   ',
+    email: 'MYEMAIL@MAPH.IO    '
+})
 
-// me.save().then((me) => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('Error!', error)
-// })
+me.save().then((me) => {
+    console.log(me)
+}).catch((error) => {
+    console.log('Error!', error)
+})
 
 const Tasks = mongoose.model('Tasks', {
     description: {
@@ -34,13 +54,13 @@ const Tasks = mongoose.model('Tasks', {
     }
 })
 
-const task = new Tasks({
-    description: 'Laundry',
-    completed: false
-})
+// const task = new Tasks({
+//     description: 'Laundry',
+//     completed: false
+// })
 
-task.save().then((task) => {
-    console.log(laundry)
-}).catch((error) => {
-    console.log('Error!', error)
-})
+// task.save().then((task) => {
+//     console.log(laundry)
+// }).catch((error) => {
+//     console.log('Error!', error)
+// })
