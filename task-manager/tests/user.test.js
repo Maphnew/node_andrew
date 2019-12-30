f5801e52d5a449d2dcc42f598d93f509f0843c60
maphnew <zcm3118@gmail.com>
2019-12-30 23:51:25 +0900

    150. Bonus: Extra Test Ideas

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -112,3 +112,11 @@ test('Should not update invalid user fields', async () => {
         })
         .expect(404)
 })
+
+//
+// User Test Ideas
+//
+// Should not signup user with invalid name/email/password
+// Should not update user if unauthenticated
+// Should not update user with invalid name/email/password
+// Should not delete user if unauthenticated
